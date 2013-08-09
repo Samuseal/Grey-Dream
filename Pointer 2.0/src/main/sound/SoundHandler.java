90ff89da642cc35bcd949195c2dfe5cc2136244e
davidjfhearnden@gmail.com <davidjfhearnden@gmail.com>
2013-08-09 23:20:08 +0100

    Wrote sound handler and transfered lasers to it

---
+++ Pointer 2.0/src/main/sound/SoundHandler.java
@@ -0,0 +1,25 @@
+package main.sound;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+
+import main.Main;
+
+public class SoundHandler {
+	public static synchronized void playSoundLaser(final String url) {
+	    try {
+	      Clip clip = AudioSystem.getClip();
+	      AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+	      Main.class.getResourceAsStream("../" + url));
+	      clip.open(inputStream);
+	      FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+	     gainControl.setValue(-20.0f); // Reduce volume by 40 decibels.
+	     if (!clip.isRunning())
+	    	 clip.start();
+	    } catch (Exception e) {
+	      System.err.println(e.getMessage());
+	    }
+	}
+}
